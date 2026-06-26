09e049fe152f695e2d029691395e1b244a6cbc79
Devraj Singh Chouhan <145596316+Devworld03@users.noreply.github.com>
2026-06-26 17:56:27 +0530

    Use environment variables for database credentials
    Refactor database connection to use environment variables.

--- database_connection.py
+++ database_connection.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-DB_USERNAME="postgres"
-DB_PASSWORD="123456789"
-HOST="localhost"
-PORT=5432
-DATABASE="student_db"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DATABASE = os.getenv("DATABASE")
 engine=create_engine(
         f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{HOST}:{PORT}/{DATABASE}"
        )
